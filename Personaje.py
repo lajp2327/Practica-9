ca7baddf33fd7f300128a88aeca0c68060e84418
lajp2327 <125925949+lajp2327@users.noreply.github.com>
2023-02-24 16:34:01 -0600

    Se hicieron los atributos privados y agregar get y set

--- Personaje.py
+++ Personaje.py
@@ -2,19 +2,35 @@ class personaje:
     #Constructor  de personaje
     def __init__(self,esp,nom,alt):
     #atributos personaje
-        self.especie=esp
-        self.nombre=nom
-        self.altura=alt
+        self.__especie=esp
+        self.__nombre=nom
+        self.__altura=alt
     
     #métodos personaje
     def correr(self, status):
         if (status):
-            print("El personaje" + self.nombre + " está corriendo")
+            print("El personaje" + self.__nombre + " está corriendo")
         else:
-            print("El personaje " + self.nombre + " se detuvo")
+            print("El personaje " + self.__nombre + " se detuvo")
     def lanzar_granadas(self):
-        print("El personaje" + self.nombre + " lanzó una granada")
+        print("El personaje" + self.__nombre + " lanzó una granada")
     def recargar_arma(self, municiones):
         cargador=10
         cargador=cargador+municiones
         print("Tu arma tiene " + str(cargador) + " balas")
+    def __pensar(self):
+        print("Estoy pensando...")
+        
+    #Getters y Setters
+    def getnombre(self):
+        return self.__nombre
+    def setnombre(self,nom):
+        self.__nombre=nom
+    def getespecie(self):
+        return self.__especie
+    def setespecie(self,esp):
+        self.__nombre=esp
+    def getaltura(self):
+        return self.__altura
+    def setnombre(self,alt):
+        self.__nombre=alt
