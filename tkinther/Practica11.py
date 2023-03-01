f9a42a3818d1be821b2dfb82b6da9e3aa6971560
lajp2327 <125925949+lajp2327@users.noreply.github.com>
2023-03-01 16:20:52 -0600

    Funciones con botones (Tkinter)

--- tkinther/Practica11.py
+++ tkinther/Practica11.py
@@ -1,8 +1,17 @@
 from tkinter import Tk,Button,Frame, messagebox
 
+#Declarar funciones para mensajes
 def mostrarmensajes():
     messagebox.showinfo("Botón azul", "Presionaste el botón azul")
-    
+    messagebox.showerror("Error" , "Todo falló con éxito")
+    print(messagebox.askyesnocancel("Pregunta" , "¿Ella jugó con tu corazón?"))
+
+#Función para agregar botones
+def agregarBoton():
+    GreenButton.config(text="+", bg="green", fg="white")
+    NewButton=Button(seccion3, text="Nuevo")
+    NewButton.pack()
+
 #Instancia de objeto ventana
 ventana= Tk()
 ventana.title("Ejemplo de 3 frames")
@@ -22,9 +31,12 @@ Bluebutton.place(x=60, y=60)
 
 YellowButton= Button(seccion2, text="Botón amarillo", fg="black", bg="yellow" )
 YellowButton.grid(row=1, column=0)
+
 BlackButton=  Button(seccion2, text="Botón negro", fg="white", bg="black")
 BlackButton.grid(row=0 , column=0)
-GreenButton= Button(seccion3, text="Botón verde", bg="green", fg="white", height=2, width=10)
+
+GreenButton= Button(seccion3, text="Botón verde", bg="green", fg="white", height=2, width=10, command=agregarBoton)
 GreenButton.pack()
+
 #llamamos al Main
 ventana.mainloop()
